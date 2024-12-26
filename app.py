997fbfd07bef15ef942ef42ba4ed7a213e45bf14
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-12-26 13:28:05 +0000

    pdf add

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ page_bd_img = """
 <style> 
         [data-testid="stAppViewContainer"]{
         background-color: #F8F4FF;
-        background-image: radial-gradient(#444cf7 1px, #9BDDFF 1px);
+        background-image: radial-gradient(#F8F4FF 1px, #9BDDFF 1px);
         background-size: 30px 30px;
         }
         </style>
