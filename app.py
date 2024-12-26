93cc5824d6820ab577f4b0ee3818d1ba670c0957
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-12-26 13:25:16 +0000

    pdf add

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ def send_to_whatsapp(conversation_log):
 
 page_bd_img = """
 <style> 
-        [data-testid="stAppViewContainer"],{
+        [data-testid="stAppViewContainer"]{
+        background-color: #e5e5f7;
         background-image: radial-gradient(#444cf7 1px, #9BDDFF 1px);
         background-size: 30px 30px;
         }
